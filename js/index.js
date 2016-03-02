b2f0de7adab3e416270a2e41783c65a4a1e3ac12
Caique Santana <caique.santana10.cs@gmail.com>
2016-03-02 04:39:59 -0300

    Atualizacao na verificacao de dados

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 ;(function(window, document, undefined) {
 	'use strict';
+
 	var iframe = document.querySelector('.result__window'),
   		ifrw = iframe.contentWindow || iframe.contentDocument.document || iframe.contentDocument,
 			saveCompleteButton = document.querySelector('.saveComplete'),
@@ -9,68 +10,97 @@
 			clearCode = document.querySelector('.clear'),
 			openWindow = document.querySelector('.openWindow'),
 			selector = '';
+
 	var addContent = function addContent(e) {
 		ifrw.document.open();
 		ifrw.document.write(textArea.value || '<h3 style="font-family: sans-serif; color: gray;">O resultado ficará aqui...</h3>'); 
   	ifrw.document.close();
   	return textArea.value;
 	};
+
 	var saveComplete = function saveComplete() {
 		if(textArea.value) {
 			var file = new Blob([textArea.value], {type: 'text/plain; charset=utf-8'});
-			saveAs(file, 'dados.html');
-		} else {
+			var nameFile = prompt('Digite o nome do arquivo (sem extensão):');
+			if(nameFile) {
+				saveAs(file, nameFile + '.html');
+				alert('Arquivo criado com sucesso!');
+			}else {
+				alert('Arquivo não pode ser criado.');
+			}
+		}else {
 			window.alert('Não foi possível criar arquivo vazio.');
 		}
 	};
+
 	var saveCSS = function saveCSS() {
-		var content = textArea.value.split('style>')[1];
-		if(content) {
-			var file = new Blob([content.replace('</', '').trim()], {type: 'text/plain; charset=utf-8'});
-			saveAs(file, 'style.css');
-		} else {
+		var textValue = textArea.value.trim();
+		var regCss = new RegExp("<style>([^<]+)\</style>");
+		if(regCss.exec(textValue)) {
+			var file = new Blob([regCss.exec(textValue)[1].trim()], {type: 'text/plain; charset=utf-8'});
+			var nameFile = prompt('Digite o nome do arquivo (sem extensão):');
+			if(nameFile) {
+				saveAs(file, nameFile + '.css');
+				alert('Arquivo criado com sucesso!');
+			}else {
+				alert('Arquivo não pode ser criado.');
+			}
+		}else {
 			window.alert('Não encontramos nenhum estilo em seu código.');
 		}
 	};
+	
 	var saveJS = function saveJS() {
-		var content = textArea.value.split('script>')[1];
-		if(content) {
-			this.style.display = 'block';
-			var file = new Blob([content.replace('</', '').trim()], {type: 'text/plain; charset=utf-8'});
-			saveAs(file, "script.js");
+		var textValue = textArea.value.trim();
+		var regJs = new RegExp("<script>([^<]+)\</script>");
+		if(regJs.exec(textValue)) {
+			var file = new Blob([regJs.exec(textValue)[1].trim()], {type: 'text/plain; charset=utf-8'});
+			var nameFile = prompt('Digite o nome do arquivo (sem extensão):');
+			if(nameFile) {
+				saveAs(file, nameFile + '.js');
+				alert('Arquivo criado com sucesso!');
+			}else {
+				alert('Arquivo não pode ser criado.');
+			}
 		}else {
 			window.alert('Não encontramos nenhum script em seu código');
 		}
 	};
+
 	var scroll = function scroll() {
 		(document.body.scrollTop += 2) >= document.querySelector('.header').clientHeight ? 
 			document.body.scrollTop = document.querySelector('.header').clientHeight : setTimeout(scroll, 10); 
 	}; 
+
 	var contentWindowDefault = function contentWindowDefault() {
 		ifrw.document.open(); 
 		ifrw.document.write('<h3 style="font-family: sans-serif; color: gray;">O resultado ficará aqui...</h3>'); 
 		ifrw.document.close();
-	};
-	contentWindowDefault();
-	saveCompleteButton.addEventListener('click', saveComplete, false);
-	saveCSSButton.addEventListener('click', saveCSS, false);
-	saveJSButton.addEventListener('click', saveJS, false);
-	textArea.addEventListener('keyup', addContent, false);
-	// textArea.addEventListener('keydown', tabIndent, false);
-	textArea.addEventListener('click', scroll, false);
+	};		
+
 	clearCode.addEventListener('click', function() {
  		textArea.value = '';
  		textArea.focus();
  		contentWindowDefault();
  		scroll();
 	}, false);
+
 	openWindow.addEventListener('click', function() {
 		var myWindow = window.open('','_blank');
 		myWindow.document.write(addContent());
 	});
+
 	emmet.require('textarea').setup({
     pretty_break: true, 
     use_tab: true       
 	});
+
+	saveCompleteButton.addEventListener('click', saveComplete, false);
+	saveCSSButton.addEventListener('click', saveCSS, false);
+	saveJSButton.addEventListener('click', saveJS, false);
+	textArea.addEventListener('click', scroll, false);
+	textArea.addEventListener('keyup', addContent, false);
+
+	contentWindowDefault();
 })(window, document);
 
